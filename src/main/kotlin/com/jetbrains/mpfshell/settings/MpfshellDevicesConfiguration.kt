ffb2a015ce9db18697507d8b813c3cadd3911153
Juwan <32978053+junhuanchen@users.noreply.github.com>
2018-10-23 10:32:44 +0800

    Update src/main/kotlin/com/jetbrains/mpfshell/settings/MpfshellDevicesConfiguration.kt

--- src/main/kotlin/com/jetbrains/mpfshell/settings/MpfshellDevicesConfiguration.kt
+++ src/main/kotlin/com/jetbrains/mpfshell/settings/MpfshellDevicesConfiguration.kt
@@ -8,7 +8,7 @@ import com.intellij.util.xmlb.annotations.Attribute
 /**
  * @author vlan
  */
-@State(name = "MicroPythonDevices", storages = arrayOf(Storage(StoragePathMacros.WORKSPACE_FILE)))
+@State(name = "MpfshellDevices", storages = arrayOf(Storage(StoragePathMacros.WORKSPACE_FILE)))
 class MpfshellDevicesConfiguration : PersistentStateComponent<MpfshellDevicesConfiguration> {
 
   companion object {
